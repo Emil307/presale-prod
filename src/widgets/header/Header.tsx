87e83f13235287038773aee1d8a9b2575b3e091c
Musya <musya.ilyasova@getorca.app>
2024-06-20 16:51:27 +0300

    [Chore] add menu

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../../assets/icons/logo.svg';
 import darkTheme from '../../assets/icons/dark-theme.svg';
 import menu from '../../assets/icons/menu.svg';
+import close from '../../assets/icons/close.svg';
 import {
   Page,
   Container,
@@ -15,12 +16,23 @@ import {
   MenuButton,
 } from './styles';
 
+
 export const Header: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow= 'hidden';
+    } else {
+      document.body.style.overflow= '';
+    }
+  }, [isOpen]);
+
   return (
     <Page>
       <Container>
         <Logo src={logo} alt='punks'/>
-        <Right>
+        <Right className={isOpen ? 'open': ''}>
           <Nav>
             <Link>Home</Link>
             <Link>Roadmap</Link>
@@ -32,7 +44,9 @@ export const Header: React.FC = () => {
             <ThemeButton><img src={darkTheme} alt="Dark theme" /></ThemeButton>
           </Buttons>
         </Right>
-        <MenuButton><img src={menu} alt="open menu" /></MenuButton>
+        <MenuButton open={isOpen} onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? <img src={close} alt="close menu" /> : <img src={menu} alt="open menu" />}
+        </MenuButton>
       </Container>
     </Page>
   )
